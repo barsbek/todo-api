7bafce91f02aa7c08c6cc141e01d9bda47b5b8ba
Unknown <mak.bekmamat@gmail.com>
2018-02-05 04:20:23 +0500

    user user_params and permit pass confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,21 +18,21 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def authenticate
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(user_params[:email])
     if @user
-      if @user.authenticate(params[:password])
-        render json: @user
+      if @user.authenticate(user_params[:password])
+        render json: @user.token
       else
         render json: @user.errors, status: :unprocessable_entity
       end
     else
-      head :unprocessable_entity
+      render json: { message: "User is not found" }, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
