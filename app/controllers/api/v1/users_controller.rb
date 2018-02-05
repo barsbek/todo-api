ae35e33a8aaba42ed0bc56af75b1ef1d53b49380
Unknown <mak.bekmamat@gmail.com>
2018-02-05 21:40:39 +0500

    remove spare actions and routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,8 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def create
+  def register
     @user = User.new(user_params)
     if @user.save
-      render status: :created
+      render :user, status: :created
     else
       # move errors into view
       render json: @user.errors, status: :unprocessable_entity
@@ -21,7 +13,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     @user = User.find_by_email(user_params[:email])
     if @user
       if @user.authenticate(user_params[:password])
-        render json: @user.token
+        render :user
       else
         render json: @user.errors, status: :unprocessable_entity
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
       namespace :users do
+        post :register
         post :authenticate
       end
     end
