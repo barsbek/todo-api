7a4bab1e774b928858db511a262f8213e55444f9
barsbek <mak.bekmamat@gmail.com>
2018-02-06 04:11:42 +0500

    create methods to check authorization

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::BaseController < ActionController::API
+  def request_auth!
+    unless current_user
+      response = { message: "User is not authorized" }
+      render json: response, state: :unauthorized
+    end
+  end
+
+  private
+    def current_user
+      payload = User.decode_token(params[:token])
+      @current_user ||= User.find_by_id(payload[:id]) if payload
+    rescue
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,24 @@
 class User < ApplicationRecord
-  has_secure_token
   has_secure_password
 
   def encode_token
-    JWT.encode(token_payload, secret)
+    JWT.encode(token_payload, User::secret)
   end
 
-  def decode_token(token)
-    JWT.decode(token, secret)
+  def self.decode_token(token)
+    body = JWT.decode(token, secret)
+    HashWithIndifferentAccess.new body[0]
+  rescue
+    # TODO: learn about proper way of handling errors
   end
 
   private
-    def secret
+    def self.secret
       Rails.application.secrets.secret_key_base
     end
 
     def token_payload
-      exp = 1.minute.from_now.to_i
+      exp = 1.hour.from_now.to_i
       { exp: exp, id: id }
     end
 end
